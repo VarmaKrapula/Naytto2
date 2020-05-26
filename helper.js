20c368d9364c78b640e64268bc2ec0208597bc0c
VarmaKrapula <59605486+VarmaKrapula@users.noreply.github.com>
2020-05-26 14:29:34 +0300

    Lisäsin muut tarpeelliset jutut

---
+++ helper.js
@@ -0,0 +1,9 @@
+
+function OpenPage(sivu)
+{
+	document.getElementById('mainFrame').setAttribute("src",sivu + ".htm");
+}
+function OpenPage(sivu)
+{
+	document.getElementById('vasenFrame').setAttribute("src",sivu + ".htm");
+}
